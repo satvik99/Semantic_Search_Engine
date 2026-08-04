33bc11dc739b4d1e153d50c9077246388d1b15c1
Satvik <jasatvik@gmail.com>
2026-08-04 11:48:58 +0530

    text extraction from pdf for future chunking

--- app/services/pdf_service.py
+++ app/services/pdf_service.py
@@ -1,5 +1,6 @@
 from fastapi import UploadFile
 from pathlib import Path
+from pypdf import PdfReader
 
 def save_pdf(file:UploadFile):
     upload_dir = Path("uploads")
@@ -8,3 +9,13 @@ def save_pdf(file:UploadFile):
     with open(file_path, "wb") as buffer:
         buffer.write(file.file.read())
     return file_path
+
+def extract_text(path:Path):
+    reader = PdfReader(path)
+    text_from_pdf = ""
+    for page in reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text_from_pdf += page_text + "\n"
+    return text_from_pdf
+
